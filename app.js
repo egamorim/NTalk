8de621e9f70be5067f37712eb5980e66a08b7e43
Eduardo Amorim <eamorim@fastsolutions.com.br>
2015-05-29 11:27:13 -0300

    commit inicial

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express')
+	, load = require('express-load')
+	, bodyParser = require('body-parser')
+	, cookieParser = require('cookie-parser')
+	, expressSession = require('express-session')
+	, methodOverride = require('method-override')
+	, error = require('./middlewares/error')
+	, app = express()
+	, server = require('http').Server(app)
+	, io = require('socket.io')(server)
+;
+
+app.set('views', __dirname + '/views');
+app.set('view engine','ejs');
+app.use(cookieParser('ntalk'));
+app.use(expressSession());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/public'));
+
+load('models')
+	.then('controllers')
+	.then('routes')
+	.into(app);
+
+io.sockets.on('connection', function(client){
+	cliet.on('send-server', function(data){
+		var msg = "<b> " + data.nome + " : </b> " + data.msg;
+		client.emit('send-client', msg);
+		client.broadcast.emit('send-client', msg);
+	});
+});
+
+app.use(error.notFound);
+app.use(error.serverError);
+
+app.listen(3000, function(){
+	console.log("Ntalk is on.")
+})
